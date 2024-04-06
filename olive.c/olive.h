8dade2166ec892016335ab07cbb789a1faf6c1e7
Sakib <sakibul706@gmail.com>
2024-04-06 14:11:42 +0600

    circle working

---
+++ olive.c/olive.h
@@ -0,0 +1,80 @@
+#ifndef OLIVE_H
+#define OLIVE_H
+
+#include <stdio.h>
+#include <sys/types.h>
+
+void fill(u_int32_t *pixels, int width, int height, u_int32_t color) {
+  for (int i = 0; i < width * height; ++i) {
+    pixels[i] = color;
+  }
+}
+
+void fill_rect(u_int32_t *pixels, u_int32_t pixels_width,
+               u_int32_t pixels_height, int x0, int y0, u_int32_t w,
+               u_int32_t h, u_int32_t color) {
+  for (int dy = 0; dy < h; dy++) {
+    int y = y0 + dy;
+    if (0 <= y && y < (int)pixels_height) {
+      for (int dx = 0; dx < w; dx++) {
+        int x = x0 + dx;
+        if (0 <= x && x < (int)pixels_width) {
+          pixels[y * pixels_width + x] = color;
+        }
+      }
+    }
+  }
+}
+
+void file_cir(u_int32_t *pixels, u_int32_t pixels_width,
+              u_int32_t pixels_height, int cx, int cy, int r, u_int32_t color) {
+  int x1 = cx - r;
+  int y1 = cy - r;
+  int x2 = cx + r;
+  int y2 = cy + r;
+
+  for (int y = y1; y <= y2; y++) {
+    if (0 > y && y >= (int)pixels_height)
+      continue;
+    for (int x = x1; x <= x2; x++) {
+      if (0 > x && x >= (int)pixels_width)
+        continue;
+      int dx = x - cx;
+      int dy = y - cy;
+      if (dx * dx + dy * dy <= r * r) {
+        pixels[y * pixels_width + x] = color;
+      }
+    }
+  }
+}
+
+int save_to_file(const u_int32_t *pixels, int width, int height,
+                 const char *file_name) {
+  FILE *f = fopen(file_name, "w");
+  if (f == NULL) {
+    return -1;
+  }
+
+  fprintf(f, "P6\n%d %d\n255\n", width, height);
+
+  for (int i = 0; i < width * height; ++i) {
+    u_int32_t color = pixels[i];
+
+    u_int8_t bytes[3] = {
+        color & 0xFF,
+        (color >> 8) & 0xFF,
+        (color >> 16) & 0xFF,
+    };
+
+    fwrite(bytes, sizeof(bytes), 1, f);
+    if (ferror(f)) {
+      fclose(f);
+      return -1;
+    }
+  }
+
+  fclose(f);
+  return 0;
+}
+
+#endif // OLIVE_INPLIMENTATRION
--- olive.c/ppm.c
+++ olive.c/ppm.c
@@ -1,79 +1,41 @@
-#include <err.h>
+#include "olive.h"
 #include <stdio.h>
-#include <sys/types.h>
 
-#define WIDTH 400
+#define WIDTH 800
 #define HEIGHT 600
+
+#define BACLGROUD_COLOR 0xFF202020
+#define FOREGROUD_COLOR 0xFF0000FF
 
 u_int32_t pixels[HEIGHT * WIDTH];
 
-void fill(u_int32_t *pixels, int width, int height, u_int32_t color) {
-  for (int i = 0; i < width * height; ++i) {
-    pixels[i] = color;
-  }
-}
-
-void fill_rect(u_int32_t *pixels, u_int32_t pixels_width,
-               u_int32_t pixels_height, int x0, int y0, u_int32_t w,
-               u_int32_t h, u_int32_t color) {
-
-  for (int dy = 0; dy < h; dy++) {
-    int y = y0 + dy;
-    if (0 <= y && y < (int)pixels_height) {
-      for (int dx = 0; dx < w; dx++) {
-        int x = x0 + dx;
-        if (0 <= x && x < (int)pixels_width) {
-          pixels[y * pixels_width + x] = color;
-        }
+// for the cheker board
+#define COLS 8
+#define ROWS 6
+#define CELL_WIDTH (WIDTH / COLS)
+#define CELL_HEIGHT (HEIGHT / ROWS)
+// checkerboard(0xFFAAFAFF, 0xFF0A0A0B);
+void checkerboard(u_int32_t color1, u_int32_t color2) {
+  for (int y = 0; y < ROWS; ++y) {
+    for (int x = 0; x < COLS; ++x) {
+      u_int32_t color;
+      if ((x + y) % 2 == 0) {
+        color = color1;
+      } else {
+        color = color2;
       }
+      fill_rect(pixels, WIDTH, HEIGHT, x * CELL_WIDTH, y * CELL_HEIGHT,
+                CELL_WIDTH, CELL_HEIGHT, color);
     }
   }
-}
-
-int save_to_file(const u_int32_t *pixels, int width, int height,
-                 const char *file_name) {
-  FILE *f = fopen(file_name, "w");
-  if (f == NULL) {
-    return -1;
-  }
-
-  fprintf(f, "P6\n%d %d\n255\n", width, height);
-
-  for (int i = 0; i < width * height; ++i) {
-    u_int32_t color = pixels[i];
-
-    u_int8_t bytes[3] = {
-        color & 0xFF,
-        (color >> 8) & 0xFF,
-        (color >> 16) & 0xFF,
-    };
-
-    fwrite(bytes, sizeof(bytes), 1, f);
-    if (ferror(f)) {
-      fclose(f);
-      return -1;
-    }
-  }
-
-  fclose(f);
-  return 0;
 }
 
 int main() {
   const char *ouput = "out.ppm";
-  fill(pixels, WIDTH, HEIGHT, 0xFF100101);
-  fill(pixels, WIDTH, HEIGHT, 0xFFFFFFFF);
-
-  int rw = 50;
-  int rh = 30;
-  fill_rect(pixels, WIDTH, HEIGHT, WIDTH / 2 - (rw / 2), HEIGHT / 2 - (rh / 2),
-            rw, rh, 0xFFb0a0FF);
-
-  fill_rect(pixels, WIDTH, HEIGHT, WIDTH / 2, 0, 1, HEIGHT, 0xFF000000);
-  fill_rect(pixels, WIDTH, HEIGHT, 0, HEIGHT / 2, WIDTH, 1, 0xFF000000);
-
+  fill(pixels, WIDTH, HEIGHT, BACLGROUD_COLOR);
+  // checkerboard(0xFFAAFAFF, BACLGROUD_COLOR);f
+  file_cir(pixels, WIDTH, HEIGHT, WIDTH / 2, HEIGHT / 2, 60, FOREGROUD_COLOR);
   if (save_to_file(pixels, WIDTH, HEIGHT, ouput) < 0) {
     printf("err: while saveing file\n");
   }
-  //
 }
